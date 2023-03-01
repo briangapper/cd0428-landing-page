be0b6794b69aa5522cda491d7aea231a8565fef0
briangapper <46603246+briangapper@users.noreply.github.com>
2023-03-01 12:41:23 +0100

    Project Completion

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 ## Table of Contents
 
-* [Instructions](#instructions)
+* [Instructions]
+* [Development]
+* [Dependencies]
 
 ## Instructions
 
@@ -11,3 +13,49 @@ The starter project has some HTML and CSS styling to display a static version of
 To get started, open `js/app.js` and start building out the app's functionality
 
 For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+
+## Development
+
+### JavaScript
+
+The JavaScript file has been structured in the following code:
+
+* [GLOBAL_VARIABLE] => variables that are used in multiple functions.
+* [BUILDER] => js code that builds HTML elements, in this case our navigation bar.
+* [FUNCTIONS] => functions that ensure smooth scrolling where needed and and addition of active classes.
+* [EVENTS] => siv different event listeners.
+* [PERFORMANCE] => performance functions.
+
+### CSS
+
+The CSS files has been strcutured in the following code:
+
+* [BODY_General] => general rules for body.
+* [TYPOGRAPHY_General] => general rules for typography.
+* [LAYOUT_General] => general rules for layout.
+* [STYLES_General] => specific styling of all elements (header, navbar, collapsible, btn, footer).
+* [RULES_General] => rules for pseudo-elements.
+* [STATE_RULES] => rules for active state elements.
+
+### HTML
+
+The HTML has been added with the following code:
+
+- Each section also has an upper button sibling element, which represents the collapsible.
+- At the bottom of all sections, a scroll-to-top anchor has been added.
+
+## Dependencies
+
+Some code of this project has been insipired by UdaCity's Knowledge Forum, such as:
+
+- makeActiv() function
+- hide navigation bar when not scrolling
+
+The rest of the code is a compilation of a lot of internet research with various examples tailored to the
+the requirements of this project.
+
+## Unanswered Questions
+
+the project has been designed so that as many requirements as possible are met. However, there are still few unanswered questions:
+
+1. In the development strategy, it is asked if there is an event listener set up for building navigation. How could this be beneficial? Whether or not sections exist in the HTML file is already somewhat of a condition for the navigation bar to be built.
--- js/app.js
+++ js/app.js
@@ -1,62 +1,204 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
+/*
+Manipulating the DOM exercise.
+Exercise programmatically builds navigation,
+scrolls to anchors from navigation,
+and highlights section in viewport upon scrolling.
+ 
+Dependencies: None
+
+JS Version: ES2015/ES6
+
+JS Standard: ESlint
 */
 
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
+/* ------------------------------------------------------- */
+/* 1) GLOBAL VARIABLES */
+/* ------------------------------------------------------- */
+const startingTime = performance.now();
+const allSections = document.querySelectorAll('section');
+
+/* ------------------------------------------------------- */
+/* 2) BUILDER */
+/* ------------------------------------------------------- */
+
+/*  2-1) NAV: Build navigation bar */
+for(let i = 0; i < allSections.length; i++){
+
+    const navUl = document.querySelector('#navbar__list');
+
+    let navLi = document.createElement('li');
+    navLi.className = 'menu__link';
+
+    let navLink = document.createElement('a');
+    let navLinkNumber = i + 1;
+    
+    navLink.href = '#section' + String(navLinkNumber);
+    navLink.textContent = allSections[i].getAttribute('data-nav');
+
+    navLi.appendChild(navLink);
+    navUl.appendChild(navLi);
+}
+
+/* ------------------------------------------------------- */
+/* 3) FUNCTIONS */
+/* ------------------------------------------------------- */
+
+/*  3-1) makeActiv(): add 'active' class to section and nav-item when in viewport */
+function makeActive(){
+
+    for (const section of allSections){
+
+        const box = section.getBoundingClientRect();
+
+        if (box.top <= 150 && box.bottom >= 150){
+
+            section.classList.add('active');
+            let navLink = document.querySelector(`a[href='#${section.id}']`);
+
+            navLink.classList.add('active');
+            navLink.parentElement.classList.add('active');
+
+        } else {
+
+            section.classList.remove('active');
+            let navLink = document.querySelector(`a[href='#${section.id}']`);
+
+            navLink.classList.remove('active');
+            navLink.parentElement.classList.remove('active');
+        }
+    }
+}
+
+/*  3-2) scrollToSection(): scroll smoothly to section on nav item click */
+function scrollToSection(link, e){
+
+    e.preventDefault();
+
+    const destination = document.querySelector(link.getAttribute('href'));
+    let coll = document.getElementById(`coll_${destination.id}`);
+    let section = coll.nextElementSibling;
+
+    /* Expand section if collapsed */
+    if(!(coll.classList.contains('active_coll'))){
+
+        section.style.maxHeight = section.scrollHeight + 'px';
+
+    }
+    
+    destination.scrollIntoView({
+        behavior: 'smooth'
+    });
+    
+}
+
+/*  3-3) scrollToTop(): scroll smoothly to the top of the page */
+function scrollToTop(e){
+
+    e.preventDefault();
+
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
+
+/* ------------------------------------------------------- */
+/* 4) EVENTS */
+/* ------------------------------------------------------- */
+
+/*
+    4-1) Scroll event: add class 'active' to section and nav-item when
+         near top of viewport
+*/       
+document.addEventListener('scroll', () => {
+    makeActive();
+});    
+
+/*  4-2) Click event: scroll smoothly from navitem to section */
+document.querySelectorAll('.menu__link a').forEach((link) => {
+
+    link.addEventListener('click', (e) => {
+        scrollToSection(link, e);
+    });
+
+});
+
+/*  4-3) Scroll event: hide navigation bar when not scrolling */
+let timer = null;
+
+document.addEventListener('scroll', () => {
+
+    const hideNav = document.querySelector('nav');
+
+    if(timer !== null) {
+        clearTimeout(timer);
+        hideNav.style.display = 'block';
+    }
+
+    timer = setTimeout( () => {
+        hideNav.style.display = 'none';
+    }, 10000);
+
+});
+
+/* 
+    4-4) Click event: add a smooth scrolling behaviour to
+         scroll-to-top button
 */
+const scrollToTopBtn = document.querySelector('#scrollToTop');
 
-/**
- * Define Global Variables
- * 
+scrollToTopBtn.addEventListener('click', (e) => {
+    scrollToTop(e);
+});
+
+/*  
+    4-5) Scroll event: Make the scroll-to-top button only visible when the
+         user finds himself at the bottom of the page
 */
+const scrollToTopDiv = document.querySelector('.scrollToTopDiv');
 
+window.addEventListener('scroll', () => {
 
-/**
- * End Global Variables
- * Start Helper Functions
- * 
-*/
+    if((window.innerHeight + window.scrollY) === document.body.scrollHeight){
+        scrollToTopDiv.style.display = 'table';
 
+    } else {
+        scrollToTopDiv.style.display = 'none';
+    }
 
+});
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+/*  4-6) Click event: Make sections collapsible */
+document.querySelectorAll('.collapsible_btn').forEach(coll => {
 
-// build the nav
+    coll.addEventListener('click', () => {
 
+        coll.classList.toggle('active_coll');
+        let section = coll.nextElementSibling;
 
-// Add class 'active' to section when near top of viewport
+        if(coll.classList.contains('active_coll')){
+            section.style.maxHeight = section.scrollHeight + 'px';
 
+        } else {
+            section.style.maxHeight = 0;
+        }
 
-// Scroll to anchor ID using scrollTO event
+    });
+});
 
+/* ------------------------------------------------------- */
+/* 5.) PERFORMANCE */
+/* ------------------------------------------------------- */
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
+/*  5-1) Performance tracker */
+const endingTime = performance.now();
+console.log('This code took ' + (endingTime - startingTime) + ' milliseconds.');
 
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
-
-
+/*  5-2) Logs */
+window.addEventListener('scroll', () => {
+    console.log('scrollY:' + String(scrollY));
+    console.log('innerHeight:' + String(window.innerHeight));
+    console.log('scrollY + innerHeight:' + (window.scrollY + window.innerHeight));
+    console.log('ScrollHeight:' + String(document.body.scrollHeight));
+});
